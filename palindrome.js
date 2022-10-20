4af498510d135b2853675940fcadbaba94950308
janvi170 <janvis1308@gmail.com>
2022-10-20 22:31:56 +0530

    check palindrome

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+\\check weather a string is palindrome or not
+    for(i=0;i<=N/2;i++){
+            if(str[i]===str[N-1+i]){
+                console.log("No");
+                break;
+            }
+    else{
+                console.log("Yes");
+                break;
+    }
