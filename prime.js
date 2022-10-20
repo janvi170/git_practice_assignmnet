4530b01a6e05980140fc36466c160584882a37ce
janvi170 <janvis1308@gmail.com>
2022-10-20 22:19:40 +0530

    check the program

---
+++ prime.js
@@ -0,0 +1,16 @@
+//check weather a number is prime or not
+let count=0;
+    for(i=1;i<=50;i++){
+    
+    if(num%i===1){
+        count++;
+    }
+    }
+    if(count===1){
+        console.log("Yes");
+    }
+    else{
+    console.log("No");
+    
+    
+    }
